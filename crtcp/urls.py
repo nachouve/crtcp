c8309d51dc1e1f4f58db0412bb63c7be1ef059e2
Jorge López <jlopez@cartolab.es>
2012-10-24 16:06:29 +0200

    Survey urls improved, and requirements updated.
    Now the surveys' urls make more sense, and the
    requirements include one for crowdsourcing.

--- crtcp/urls.py
+++ crtcp/urls.py
@@ -11,8 +11,9 @@ from crowdsourcing.views import (allowed_actions,
                     submission_for_map,
                     survey_detail,
                     survey_report)
-
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
@@ -31,77 +32,81 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('userena.urls')),
     ('^$', redirect_to, {'url':'/home/' }),
-    url(r'^submissions/$',
+    url(r'^survey/submissions/$',
         submissions,
         {"format": "json"},
         name='submissions'),
 
-    url(r'^submissions/(?P<format>[a-z]+)/$',
+    url(r'^survey/submissions/(?P<format>[a-z]+)/$',
         submissions,
         name='submissions_by_format'),
 
-    url(r'^submission/(?P<id>\d+)/$',
+    url(r'^survey/submission/(?P<id>\d+)/$',
         submission),
 
-    url(r'^submission_for_map/(?P<id>\d+)/$',
+    url(r'^survey/submission_for_map/(?P<id>\d+)/$',
         submission_for_map),
 
-    url(r'^location_question_results/(?P<question_id>\d+)/(?P<limit_map_answers>\d+)/$',
+    url(r'^survey/location_question_results/(?P<question_id>\d+)/(?P<limit_map_answers>\d+)/$',
         location_question_results,
         kwargs={"survey_report_slug": ""}),
 
-    url(r'^location_question_results/(?P<question_id>\d+)/(?P<limit_map_answers>\d*)/(?P<survey_report_slug>[-a-z0-9_]*)/$',
+    url(r'^survey/location_question_results/(?P<question_id>\d+)/(?P<limit_map_answers>\d*)/(?P<survey_report_slug>[-a-z0-9_]*)/$',
         location_question_results,
         name="location_question_results"),
 
-    url(r'^location_question_map/(?P<question_id>\d+)/(?P<display_id>\d+)/$',
+    url(r'^survey/location_question_map/(?P<question_id>\d+)/(?P<display_id>\d+)/$',
         location_question_map,
         name="location_question_map"),
 
-    url(r'^location_question_map/(?P<question_id>\d+)/(?P<display_id>\d+)/(?P<survey_report_slug>[-a-z0-9_]*)/$',
+    url(r'^survey/location_question_map/(?P<question_id>\d+)/(?P<display_id>\d+)/(?P<survey_report_slug>[-a-z0-9_]*)/$',
         location_question_map,
         name="location_question_map"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/$',
         survey_detail,
         name="survey_detail"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/api/allowed_actions/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/api/allowed_actions/$',
         allowed_actions,
         name="allowed_actions"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/api/questions/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/api/questions/$',
         questions,
         name="questions"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/api/embeded_survey_questions/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/api/embeded_survey_questions/$',
         embeded_survey_questions,
         name="embeded_survey_questions"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/api/report/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/api/report/$',
         embeded_survey_report,
         {"report": ""},
         name="embeded_survey_report_default"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/api/report/(?P<report>[-a-z0-9_]+)/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/api/report/(?P<report>[-a-z0-9_]+)/$',
         embeded_survey_report,
         name="embeded_survey_report"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/report/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/report/$',
         survey_report,
         name="survey_default_report_page_1"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/report/(?P<page>\d+)/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/report/(?P<page>\d+)/$',
         survey_report,
         name="survey_default_report"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/(?P<report>[-a-z0-9_]+)/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/(?P<report>[-a-z0-9_]+)/$',
         survey_report,
         name="survey_report_page_1"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/(?P<report>[-a-z0-9_]+)/(?P<page>\d+)/$',
+    url(r'^survey/(?P<slug>[-a-z0-9_]+)/(?P<report>[-a-z0-9_]+)/(?P<page>\d+)/$',
         survey_report,
-        name="survey_report")
+        name="survey_report"),
+
+    (r'^media/(?P<path>.*)$',
+     'django.views.static.serve',
+     {'document_root': settings.MEDIA_ROOT})
 )
 
 urlpatterns += patterns('django.contrib.flatpages.views',
